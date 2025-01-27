3ae273da18ba254bab7c697fefb6216acb86f00e
taeKrits <kritsada@ada-soft.com>
2025-01-27 14:29:53 +0700

    add libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,22 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("maven-publish")
+}
+
+ publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.github.AdaDevDroid" // GitHub Username
+            artifactId = "AAChartCore"        // ชื่อ Library
+            version = "1.0.0"              // เวอร์ชันของ Library
+            // ระบุไฟล์ .aar ที่ต้องการเผยแพร่
+            artifact("libs/AAChartCore.jar") {
+                extension = "jar"          // ระบุชนิดไฟล์
+            }
+        }
+        repositories {
+            mavenLocal()
+        }
+    }
 }
